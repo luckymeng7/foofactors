e76c1b088062396d707a2ea50410f62a6e2c757a
Xuemeng Li <luckymeng7@gmail.com>
2020-04-02 11:55:50 -0700

    commit working with two simple tests

--- R/fbind.R
+++ R/fbind.R
@@ -1,12 +1,15 @@
-#' Title
+#' Bind two factors
 #'
-#' @param a <- factors a
-#' @param b <- factors b
+#' Create a new factor from two existing factors, where the new factor's levels
+#' are the union of the levels of the input factors.
 #'
-#' @return - concatenated values
+#' @param a factor
+#' @param b factor
 #'
-#'
+#' @return factor
+#' @export
 #' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 fbind <- function(a, b) {
 	factor(c(as.character(a), as.character(b)))
 }
